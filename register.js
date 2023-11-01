45c5b67ee7649d9716dae44fadc4b9cf77224b5c
selamierdn <erdenselami07@gmail.com>
2023-11-01 22:40:48 +0300

    ok

--- register.js
+++ register.js
@@ -5,24 +5,29 @@ $(document).ready(function() {
 
         window.location.href = "index.html"
     });
+
+    $("#register-form").submit(function(event) {
+        event.preventDefault();
+      
+        let username = $("#username").val();
+        let password = $("#password").val();
+      
+        let userObject = {
+          username: username,
+          password: password
+        };
+      
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+      
+        users.push(userObject);
+      
+        localStorage.setItem("users", JSON.stringify(users));
+      
+        window.location.href = "index.html";
+      });
+      
 });
 
-document.getElementById("register-form").addEventListener("submit", function() {
- 
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-
-  let userObject = {
-      username: username,
-      password: password
-  };
-
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-
-  users.push(userObject);
-
-  localStorage.setItem("users", JSON.stringify(users));
-});
 
 
 
